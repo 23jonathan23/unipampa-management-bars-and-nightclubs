82aea87d37a151047d82782403c7e2343dd21f91
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-04-20 22:21:02 -0300

    DBRepository interface  added

--- src/main/java/edu/unipampa/poo/management/bars/and/nightclubs/Infra/Repository/DBRepository.java
+++ src/main/java/edu/unipampa/poo/management/bars/and/nightclubs/Infra/Repository/DBRepository.java
@@ -7,17 +7,20 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.unipampa.poo.management.bars.and.nightclubs.Infra.Interfaces.IDBRepository;
+
 import java.nio.file.*;
 
 @SuppressWarnings("unchecked")
-public class DBRepository<T> {
-    protected Path _pathDB;
-    protected FileOutputStream _fileOutStream;
-    protected ObjectOutputStream _objectOutStream;
-    protected FileInputStream _fileInputStream;
-    protected ObjectInputStream _objectInputStream;
-    protected List<T> _cache;
-    protected boolean _updateCache = true;
+public class DBRepository<T> implements IDBRepository<T>{
+    private Path _pathDB;
+    private FileOutputStream _fileOutStream;
+    private ObjectOutputStream _objectOutStream;
+    private FileInputStream _fileInputStream;
+    private ObjectInputStream _objectInputStream;
+    private List<T> _cache;
+    private boolean _updateCache = true;
 
     public DBRepository(Path pathBD) {
         _pathDB = pathBD;
@@ -51,38 +54,44 @@ public class DBRepository<T> {
         _updateCache = true;
     }
 
-    public void update(T entity) throws IOException, ClassNotFoundException {
+    public void update(T entity) throws IOException, ClassNotFoundException, IllegalArgumentException {
         var listEntity = queryAll();
 
         for (var entityOld : listEntity) {
             if (entityOld.equals(entity)) {
                 listEntity.set(listEntity.indexOf(entityOld), entity);
-                break;
+
+                openFileWrite(_pathDB.toString());
+
+                _objectOutStream.writeObject(listEntity);
+
+                disposeFileWrite();
+
+                return;
             }
         }
 
-        openFileWrite(_pathDB.toString());
-
-        _objectOutStream.writeObject(listEntity);
-
-        disposeFileWrite();
+        throw new IllegalArgumentException("O registro informado não existe");
     }
 
-    public void delete(T entity) throws IOException, ClassNotFoundException {
+    public void delete(T entity) throws IOException, ClassNotFoundException, IllegalArgumentException {
         var listEntity = queryAll();
 
         for (var entityOld : listEntity) {
             if (entityOld.equals(entity)) {
                 listEntity.remove(listEntity.indexOf(entity));
-                break;
+
+                openFileWrite(_pathDB.toString());
+
+                _objectOutStream.writeObject(listEntity);
+
+                disposeFileWrite();
+
+                return;
             }
         }
 
-        openFileWrite(_pathDB.toString());
-
-        _objectOutStream.writeObject(listEntity);
-
-        disposeFileWrite();
+        throw new IllegalArgumentException("O registro informado não existe");
     }
 
     public List<T> queryAll() throws IOException, ClassNotFoundException {
@@ -101,22 +110,22 @@ public class DBRepository<T> {
         return _cache;
     }
 
-    protected void openFileWrite(String file) throws IOException {
+    private void openFileWrite(String file) throws IOException {
         _fileOutStream = new FileOutputStream(file);
         _objectOutStream = new ObjectOutputStream(_fileOutStream);
     }
 
-    protected void disposeFileWrite() throws IOException {
+    private void disposeFileWrite() throws IOException {
         _fileOutStream.close();
         _objectOutStream.close();
     }
 
-    protected void openFileRead(String file) throws IOException {
+    private void openFileRead(String file) throws IOException {
         _fileInputStream = new FileInputStream(file);
         _objectInputStream = new ObjectInputStream(_fileInputStream);
     }
 
-    protected void disposeFileRead() throws IOException {
+    private void disposeFileRead() throws IOException {
         _fileInputStream.close();
         _objectInputStream.close();
     }
