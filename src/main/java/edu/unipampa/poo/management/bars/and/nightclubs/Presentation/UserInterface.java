70df926d3d24df7f7f853ade9417d5c2da9ea07b
gpmatheus <gp.matheusluis@gmail.com>
2021-04-30 19:16:51 -0300

    added to test fxml

---
+++ src/main/java/edu/unipampa/poo/management/bars/and/nightclubs/Presentation/UserInterface.java
@@ -0,0 +1,24 @@
+
+package edu.unipampa.poo.management.bars.and.nightclubs.Presentation;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application{
+    
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("UserInterface.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
