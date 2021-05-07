f5e0bc4c7d1ab848eef1c6a8d54bd79cc62aaeba
gpmatheus <gp.matheusluis@gmail.com>
2021-05-07 03:22:16 -0300

    added some controls

--- src/main/java/edu/unipampa/poo/management/bars/and/nightclubs/Presentation/UserInterfaceController.java
+++ src/main/java/edu/unipampa/poo/management/bars/and/nightclubs/Presentation/UserInterfaceController.java
@@ -7,14 +7,39 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 
 public class UserInterfaceController implements Initializable{
     
     @FXML
-    private Label texto;
+    private TabPane tab;
+    @FXML
+    private Tab clientTab;
+    @FXML
+    private TextField search;
+    @FXML
+    private Button listAll;
+    @FXML
+    private Button add;
+    
+    @FXML
+    public void toClient() {
+        if (search == null) {
+            search = new TextField("Digite o RG");
+        }
+        search.setPromptText("Digite o RG");
+    }
+    @FXML
+    public void toProduct() {
+        search.setPromptText("Digite o codigo do produto");
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        toClient();
     }
 }
