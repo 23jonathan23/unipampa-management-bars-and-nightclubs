c88f5463d6fa69cd97d6256a18165db18c938aaa
gpmatheus <gp.matheusluis@gmail.com>
2021-05-11 22:01:37 -0300

    foi adicionado um tableview na primeira tab

--- src/main/java/edu/unipampa/poo/management/bars/and/nightclubs/Presentation/UserInterfaceController.java
+++ src/main/java/edu/unipampa/poo/management/bars/and/nightclubs/Presentation/UserInterfaceController.java
@@ -1,8 +1,14 @@
 
 package edu.unipampa.poo.management.bars.and.nightclubs.Presentation;
 
+import edu.unipampa.poo.management.bars.and.nightclubs.Domain.Client;
+import edu.unipampa.poo.management.bars.and.nightclubs.Domain.ClientCabin;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
@@ -11,12 +17,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class UserInterfaceController implements Initializable{
     
     @FXML
     private TabPane tab;
+    @FXML
+    private Tab clientsTab;
     @FXML
     private Tab clientTab;
     @FXML
@@ -41,5 +52,26 @@ public class UserInterfaceController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         toClient();
+        
+        TableView tv = new TableView();
+        
+        TableColumn<Client, String> columnRg = new TableColumn<>("RG");
+        columnRg.setMinWidth(150);
+        columnRg.setCellValueFactory(new PropertyValueFactory<>("rg"));
+        
+        TableColumn<Client, String> columnName = new TableColumn<>("Nome");
+        columnName.setMinWidth(150);
+        columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        tv.getColumns().addAll(columnRg, columnName);
+        clientsTab.setContent(tv);
+        
+        Client cliente = new ClientCabin("12345", "nome dele", 45d);
+        List<Client> listaCliente = new ArrayList<>();
+        listaCliente.add(cliente);
+        
+        ObservableList<Client> ol = FXCollections.observableArrayList(listaCliente);
+        
+        tv.setItems(ol);
     }
 }
